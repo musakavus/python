7f0bc46ea552dfc7cf725d73a20e388389f01315
Musa <musakavus@gmail.com>
2024-01-10 14:43:51 +0300

    some change applied

--- 013-Args-Kwargs/main.py
+++ 013-Args-Kwargs/main.py
@@ -52,3 +52,38 @@ def my_func(sayi1, sayi2):
 
 total = my_func(5, 7)
 print(total)
+
+
+def basic_sum(*args):
+    return sum(args)
+
+
+total = basic_sum(4, 98, 121, 1)
+print(total)
+
+
+def an_example(*args):
+    return args
+
+
+return_value = an_example('it', 'is', 'an', 'example')
+print(return_value)
+print(type(return_value))  # output: <class 'tuple'>
+
+
+# dikkat edilmesi gereken şey şu--> fonksiyon eleman gönderince tuple şeklinde gönderir.
+# dolayısıyla işleme sokacağımız elemanları bir tuple gibi düşünebiliriz. Zaten o yüzden sum() fıonksiyonu işe yaradı
+
+
+# Bir fonksiyon içerisine parametre olarak hem arg hemde kwarg verilebilir. Ama sıralama çok önemlidir
+
+def info(classroom, *args, **kwargs):
+    print(f'classroom: {classroom}')
+    print(f'Marks: {args}')
+    print(f'Infos: {kwargs}')
+
+
+info('12 / C', 25, 30, 45, 78, 95, name='ruin', surname='chain')
+
+a_value = 'Django'
+print(a_value)
--- 014-Try-Except/main.py
+++ 014-Try-Except/main.py
@@ -1,37 +1,136 @@
-"""
-Error Types:
-Syntax
-Logic
-Exception
-"""
+# """
+# Error Types:
+# Syntax
+# Logic
+# Exception
 
-# except olmadan try çalışmaz.Her try'ın muhakkak bir exceptionu olmalı
+# hata konusunda olabildiğince açıklayıcı ol. Mümkünse her hatayı açıkla ki kullanıcı bilgilensin
+# """
+#
+# # except olmadan try çalışmaz.Her try'ın muhakkak bir exceptionu olmalı
+#
+# # Shift + * --> todo list
+#
+# try:
+#     number = int(input('please enter an integer number: '))
+#     print('Your entered number: ', number)
+#
+# except ValueError:
+#     print('You can entered just integer. Pls try again')
+#
+# while True:
+#     try:
+#         mark = int(input('please enter your mark: '))
+#         average = int(input('Total count: '))
+#         division = mark / average
+#         print('your entered number: ', division)
+#         break
+#     except ValueError:
+#         print('pls enter an number...')
+#     except ZeroDivisionError:
+#         print('Total count can not be Zero. Try again...')
+#
+# # TODO: Else
+#
+# while True:
+#     try:
+#         mark = int(input('please enter your mark: '))
+#         average = int(input('Total count: '))
+#         division = mark / average
+#         print('your entered number: ', division)
+#     except ValueError:
+#         print('pls enter an number...')
+#     except ZeroDivisionError:
+#         print('Total count can not be Zero. Try again...')
+#     else:
+#         print('Succeed')  # hiç bir hata bulunamadığı takdirde, ve try sorunsuz çalışırsa else çalışır
+#         # yani anlayacağın else hata olmadığı durumlarda çalışır
+#         break
+#
+# print('block-out')
 
-# Shift + * --> todo list
+# Hataları birleştirme
+# while True:
+#     try:
+#         mark = int(input('please enter your mark: '))
+#         average = int(input('Total count: '))
+#         division = mark / average
+#         print('your entered number: ', division)
+#     except (ValueError, ZeroDivisionError):  # hata birleştirme kısmı
+#         print('pls enter an number and except number zero...')
+#     else:
+#         print('Succeed')  # hiç bir hata bulunamadığı takdirde, ve try sorunsuz çalışırsa else çalışır
+#         # yani anlayacağın else hata olmadığı durumlarda çalışır
+#         break
+# # TODO: Raise
 
-try:
-    number = int(input('please enter an integer number: '))
-    print('Your entered number: ', number)
-
-except ValueError:
-    print('You can entered just integer. Pls try again')
-
-print('block-out')
-
-while True:
-    try:
-        mark = int(input('please enter your mark: '))
-        average = int(input('Total count: '))
-        division = mark / average
-        print('your entered number: ', average)
-        break
-    except ValueError:
-        print('pls enter an number...')
-    except ZeroDivisionError:
-        print('Total count can not be Zero. Try again...')
-
-print('block-out')
 
 # TODO: Else
 
-# TODO: Raise
+
+# bu şekil yazarsan hata alırsın. Çünkü try da bir hata olmadığı için bölüm işleminde 0 bölünme hatası oluyor
+# ve bundan dolayı program patlıyor. O yüzden yapılması gereken else içinde tekrar try except
+# while True:
+#     try:
+#         mark = int(input('please enter your mark: '))
+#         average = int(input('Total count: '))
+#
+#     except ValueError:
+#         print('pls enter an number...')
+#     except ZeroDivisionError:
+#         print('Total count can not be Zero. Try again...')
+#     else:
+#         division = mark / average
+#         print('your entered number: ', division)
+#         print('Succeed')  # hiç bir hata bulunamadığı takdirde, ve try sorunsuz çalışırsa else çalışır
+#         # yani anlayacağın else hata olmadığı durumlarda çalışır
+#         break
+
+
+# En doğru kullanım buu
+# while True:
+#     try:
+#         mark = int(input('please enter your mark: '))
+#         average = int(input('Total count: '))
+#     except ValueError:
+#         print('pls enter an integer number')
+#     else:
+#         try:
+#             division = mark / average
+#             print('average number: ', division)
+#             break
+#         except ZeroDivisionError:
+#             print('this function can not be dividing by 0...')
+
+# AS deyimi
+
+# total_err = 0
+# while True:
+#     try:
+#         product = (input('Enter product name: '))
+#         price = float(input('Enter a price: '))
+#
+#     except ValueError as err:
+#         total_err = total_err + 1
+#         print('Hata: ', err)
+#         # bu err değişkeninin çıktısı tuple şeklinde de alabilirsin. mesaj.args ile. Fonksiyon değil
+#     except ZeroDivisionError as err:  # hepsini err ile tanımlayabilirsin. Çünkü err burda local değişkendir
+#         print(err)
+#     else:
+#         print('Product Saved')
+#         break
+#
+# print('Total count: ', total_err)
+
+# finally hata olsa da olmasa da çalışır
+
+# try, finally olduğu sürece except olmadan da çalışır
+while True:
+    try:
+        point = int(input('Enter a number: '))
+
+    finally:
+        print('finally block always work')
+        break
+
+print('block out')
